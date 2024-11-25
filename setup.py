539c23627f1d0ca048fa65637208c756346a84a6
denywa <dwa1503@gmail.com>
2024-11-26 05:29:01 +0800

    .

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="himpunan_tim5",  
-    version="1.0.0",  
+    version="0.1.0",  
     author="Deny Wahyudi Asaloei",
-    author_email="dwa1503@gmail.com",
     description="Library Python untuk implementasi teori himpunan",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/denywa/himpunan-library",
     packages=find_packages(),
-    python_requires=">=3.6",
+    install_requires=[],
 )
