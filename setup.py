3808ea039e57df5503c8d999063dfdbbdcf110ba
denywa <dwa1503@gmail.com>
2024-11-26 06:03:56 +0800

    update

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="himpunan_tim5",  
-    version="0.1.0",  
+    version="0.1.1",  
     author="Deny Wahyudi Asaloei",
     description="Library Python untuk implementasi teori himpunan",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],
 )
